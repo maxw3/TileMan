a8b42e820dcef51d15c4fa9a6a49167e596969f2
Max Nazarchuk <maxnazarchuk@gmail.com>
2021-02-26 16:43:49 -0800

    Window!
    Created Window to contain game panel

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JOptionPane;
 
 public class Main {
 	
 	static MainFrame myMainFrame;
 	
 	public static void main(String[] args) {
+		
 		myMainFrame = new MainFrame();
-
+		
+		myMainFrame.addWindowListener(new WindowAdapter() {
+		    @Override
+		    public void windowClosing(WindowEvent windowEvent) {
+		        if (JOptionPane.showConfirmDialog(myMainFrame, 
+		            "Are you sure you want to terminate the game?", "terminado?", 
+		            JOptionPane.YES_NO_OPTION,
+		            JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
+		            System.exit(0);
+		        }
+		    }
+		});
 	}
 
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,5 +1,39 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.EventListener;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class MainFrame extends JFrame{
+public class MainFrame extends JFrame implements ActionListener, EventListener{
+	
+	private JLabel title = new JLabel("this is a mess");
+	private ImageIcon image = new ImageIcon("logo.png");
+	
+	static JPanel topPanel = new JPanel();
+	private JPanel bottomPanel = new JPanel();
+	private JPanel leftPanel = new JPanel();
+	private JPanel rightPanel = new JPanel();
+	
+	MainFrame(){
+		//----------------------------------------------------------configure window
+		this.setTitle("TileMan");
+		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		
+		this.setResizable(true);				//resizable?
+		this.setSize(1400, 1050);
+		this.setLayout(new BorderLayout(2,2));
+		
+		this.setVisible(true);
+		
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
